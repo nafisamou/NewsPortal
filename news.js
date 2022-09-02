2290667585beedc3522f1f42b263e9f07f12a9f8
nafisamou <nafisaakter621@gmail.com>
2022-09-03 02:43:16 +0600

    image-thumbnail added

--- news.js
+++ news.js
@@ -1,1 +1,24 @@
-console.log('hi')
+const loadCategory =  () => {
+    const url = `https://openapi.programming-hero.com/api/news/categories`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCategory(data.data.news_category))}
+    
+
+  const displayCategory = (categories)=>{
+   const categoriesContainer = document.getElementById('categories-container');
+   categoriesContainer.textContent = '';
+  categories.forEach(category => {
+    // console.log(category)
+    const categoriesLi= document.createElement('li');
+    categoriesLi.classList.add('nav-item')
+    categoriesLi.innerHTML = `
+            <a class="nav-link text-secondary fs-5" aria-current="page" href="#">${category.category_name}</a>
+    `
+    categoriesContainer.appendChild(categoriesLi)
+   });
+  }
+
+  loadCategory();
+
+  
